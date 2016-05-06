89b43ffcef395a0f2e281b6008e6afbbe7a7555b
bmalik8 <bmalik8@students.d125.org>
2016-05-06 08:11:34 -0500

    does this work?

---
+++ bestFinalProjectEver/Ship.java
@@ -0,0 +1,12 @@
+
+/**
+ * Ships for battle
+ * 
+ * @Basit Malik
+ * v0.0.1
+ */
+public class Ship
+{
+    private int len;
+    private int hitCtr;
+}
---
+++ bestFinalProjectEver/Spot.java
@@ -0,0 +1,13 @@
+
+/**
+ * Each position in the Array for game board
+ * 
+ * @Basit Malik 
+ * v0.0.1
+ */
+public class Spot
+{
+    private boolean containsShip;
+    private Ship battleShip;
+    public Spot
+}
