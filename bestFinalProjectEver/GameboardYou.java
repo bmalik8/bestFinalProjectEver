2b1593c27ad751b11c3163506fdaf4554952c0b7
Allison Chen 2018 <achen8@students.d125.org>
2016-05-09 08:59:51 -0500

    Working on gameboard class

---
+++ bestFinalProjectEver/GameboardYou.java
@@ -0,0 +1,29 @@
+public class GameboardYou
+{
+    private BoardSpot[][] board;
+    int shipsSunken;
+    public GameboardYou()
+    {
+        /*
+board=new BoardSpot[10][10];
+        for (int row=0; row<10; row++)
+        {
+            for (int col=0; col<10; col++)
+            {
+                board[row][col]=(false, null, false);
+            }
+        }
+        shipsSunken=0;
+    }
+    */
+    
+    public void placeShip(int x, int y, Ship ship)
+    {
+        /*
+        if (ship.getIsVert)
+        {
+            board[x][y]=new BoardSpot(true,ship
+        }
+        */
+    }
+}
